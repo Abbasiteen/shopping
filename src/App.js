2984927842e2e609a3db39fb54282923e9971d4f
Abbasiteen <104362583+Abbasiteen@users.noreply.github.com>
2022-06-30 18:28:14 +0500

    e
    e

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import Dashboard from './pages/js/Dashboard'
+import Error from './pages/js/Error'
+import Navbar from './pages/js/Navbar'
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <Navbar/>
         <Dashboard/>
-        
+        <Error/>
       </div>
     )
   }
--- src/pages/js/Navbar.js
+++ src/pages/js/Navbar.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
-
+import { BsTelephone } from 'react-icons/bs'
+import { FaMapMarkerAlt } from 'react-icons/fa'
+import "../css/Navbar.css"
 export default class Navbar extends Component {
   render() {
     return (
-      <div>Navbar</div>
+      <div>
+<div className='navbar'>
+    <div className='navbarbody'>
+       <span> <BsTelephone/>+99896487223</span>
+       <span> <FaMapMarkerAlt/>Tashkent</span>
+    </div>
+    <div className='navbarheader'></div>
+     </div>
+      </div>
     )
   }
 }
