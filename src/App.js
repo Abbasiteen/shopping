1be3a2897d06d5de3c63de783d80980ca465af93
Abbasiteen <104362583+Abbasiteen@users.noreply.github.com>
2022-06-30 18:12:45 +0500

    abbas
    inslallar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
+import Dashboard from './pages/js/Dashboard'
 
 export default class App extends Component {
   render() {
     return (
-      <div></div>
+      <div>
+        <Dashboard/>
+        
+      </div>
     )
   }
 }
