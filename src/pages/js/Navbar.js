24734628cd463549e515feda686d76488658dd47
Abbasiteen <104362583+Abbasiteen@users.noreply.github.com>
2022-06-30 17:57:39 +0500

    abbas
    navbarla

--- src/pages/js/Navbar.js
+++ src/pages/js/Navbar.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 
-export default class App extends Component {
+export default class Navbar extends Component {
   render() {
     return (
-      <div></div>
+      <div>Navbar</div>
     )
   }
 }
