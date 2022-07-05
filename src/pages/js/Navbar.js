90c0ac779109ed91f85ab87497ed39bb067526d2
diy0r4ik <95129772+diy0r4ik@users.noreply.github.com>
2022-07-05 20:14:02 +0500

    ko
    lokj

--- src/pages/js/Navbar.js
+++ src/pages/js/Navbar.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { BsTelephone } from 'react-icons/bs'
 import { FaMapMarkerAlt } from 'react-icons/fa'
+import { RiMenu4Line } from 'react-icons/ri'
+import { FiSearch } from 'react-icons/fi'
 import logo from '../img/logo.png'
 import "../css/Navbar.css"
 export default class Navbar extends Component {
@@ -16,10 +18,18 @@ export default class Navbar extends Component {
        <span> <BsTelephone/>+99896487223</span>
        <span><p className='zakazat'>Заказать звонок</p></span>
     </div>
-    <div className='navbarheader'>
-    <img src={logo} alt=""className='logo-img'/>
+  <div className='nav'>
+    <div className='logo'>
+      <img src={logo} alt=""className='logo-img'/>
     </div>
-     </div>
+    <div className='katalog'>
+      <span><RiMenu4Line/>Каталог</span>
+    </div>
+    <div className='input'>
+      <input className='header_input' placeholder='Что желаете найти?'/><FiSearch/>
+    </div>
+  </div>
+</div>
       </div>
     )
   }
