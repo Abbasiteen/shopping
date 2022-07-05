90077231800c23f87ad09870f8a642295a077297
diy0r4ik <95129772+diy0r4ik@users.noreply.github.com>
2022-07-05 14:44:49 +0500

    sdvc
    asc

--- src/pages/js/Navbar.js
+++ src/pages/js/Navbar.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react'
 import { BsTelephone } from 'react-icons/bs'
 import { FaMapMarkerAlt } from 'react-icons/fa'
+import {logo} from '../img/logo.svg'
 import "../css/Navbar.css"
 export default class Navbar extends Component {
   render() {
     return (
       <div>
 <div className='navbar'>
+  <div className='main_text'>
+    <p>О нас</p>
+    <span><FaMapMarkerAlt/>Ташкент</span>
+  </div>
     <div className='navbarbody'>
        <span> <BsTelephone/>+99896487223</span>
-       <span> <FaMapMarkerAlt/>Tashkent</span>
+       <span><p className='zakazat'>Заказать звонок</p></span>
     </div>
-    <div className='navbarheader'></div>
+    <div className='navbarheader'>
+      {/* <img src={logo} /> */}
+    </div>
      </div>
       </div>
     )
