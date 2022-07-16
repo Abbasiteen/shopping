b5d7480913f4f5e536f1ef99ed4e70ca110f7284
Abbasiteen <104362583+Abbasiteen@users.noreply.github.com>
2022-07-16 18:09:27 +0500

    sdvf
    sdfv

--- src/pages/js/Dashboard.js
+++ src/pages/js/Dashboard.js
@@ -25,8 +25,6 @@ export default class Dashboard extends Component {
   <img className="d-block w-100" src={carousel} alt="Second slide"/>
 
     <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
@@ -35,16 +33,12 @@ export default class Dashboard extends Component {
     
 
     <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
     <img className="d-block w-100" src={carousel} alt="Second slide"/>
 
     <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
     </Carousel.Caption>
   </Carousel.Item>
 </Carousel>
